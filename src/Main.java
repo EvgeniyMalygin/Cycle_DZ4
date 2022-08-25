54ba467e3e2f6e880ba4f1343802593c429354aa
Evgeniy Malygin <black2001@inbox.ru>
2022-08-25 12:37:33 +0300

    Выполнено задание №2

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,25 @@ public class Main {
         //Задание 1
         System.out.println("============================ Задание 1 =====================");
         int i = 1;
-        while (i <= 10){
+        while (i <= 10)
+        {
             System.out.print(i + " ");
             i++;
         }
         System.out.println();
         i = 10;
-        for (; i > 0; i--){
+        for (; i > 0; i--)
+        {
             System.out.print(i + " ");
+        }
+        System.out.println();
+        //Задание 2
+        System.out.println("============================ Задание 2 =====================");
+        int dataFistFriday = 1; //Дата первой пятницы
+        while (dataFistFriday <= 31)
+        {
+            System.out.println("Сегодня пятница, " + dataFistFriday + " число. Необходимо подготовить отчет.");
+            dataFistFriday = dataFistFriday + 7;
         }
     }
 }
