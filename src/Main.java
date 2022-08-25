c6e39211040c53775b408b99f0154d40c0601d9c
Evgeniy Malygin <black2001@inbox.ru>
2022-08-25 12:30:36 +0300

    Выполнено задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.text.Format;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Задание 1
+        System.out.println("============================ Задание 1 =====================");
+        int i = 1;
+        while (i <= 10){
+            System.out.print(i + " ");
+            i++;
+        }
+        System.out.println();
+        i = 10;
+        for (; i > 0; i--){
+            System.out.print(i + " ");
+        }
     }
 }
